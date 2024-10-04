1acedf526f0e553190a85f9f65e72d15eae0229e
Stefani-web <stephaniecvlho@gmail.com>
2024-10-04 14:51:11 +0200

    Add app.js file
     <Co-authored-by : Delphine-H <Delphine-H@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Welcome from './pages/welcome';
+import Login from './pages/login';
+import './styles/welcome.css';
+import './styles/login.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Route pour la page d'accueil */}
+          <Route path="/" element={<Welcome />} />
+
+          {/* Route pour la page de connexion */}
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
