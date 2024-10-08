4ee912333ff32888035dde94008a000598aa4083
Stefani-web <stephaniecvlho@gmail.com>
2024-10-08 09:22:06 +0200

    Add content files .js ( App.js, welcome.js, login.js, register.js, menu.js, index.js)
    Co-authored-by : Delphine-H <Delphine-H@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Welcome from './pages/welcome';
-import Login from './pages/login';
+import Welcome from './pages/welcome.js';
+import Login from './pages/login.js';
+import Register from './pages/register.js'
 import './styles/welcome.css';
 import './styles/login.css';
+import './styles/general.css';
 
 function App() {
   return (
@@ -16,6 +18,10 @@ function App() {
 
           {/* Route pour la page de connexion */}
           <Route path="/Login" element={<Login />} />
+
+          {/* Route pour la page de connexion */}
+          <Route path="/register" element={<Register />} />
+
         </Routes>
       </div>
     </Router>
