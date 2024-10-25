c71f3684e6828631f9eecca3b84254556649aa30
Delphine HANNON <hannon.delphine@gmail.com>
2024-10-25 11:25:26 +0200

    Add checkauth route to valide jwt.

---
+++ backend/routes/checkauth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const sequelize = require('../config/database');
+const authenticateJWT = require('../middleware/authenticateJWT');
+
+const router = express.Router();
+
+// POST : check if the user is authenticated
+router.post('/', authenticateJWT, async (req, res) => {
+  return res.status(200).json({ message: 'Authenticated' });
+}); 
+
+module.exports = router;
