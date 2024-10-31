f6d94963886e3975a9819a69f34723e084dc28ec
Delphine HANNON <hannon.delphine@gmail.com>
2024-10-31 11:48:19 +0100

    Add game page.

---
+++ frontend/src/pages/jeux.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/header";
+import { fetchUserScore } from "../utils/appUtils";
+import Game from "../components/spaceInvaders/SpaceInvader";
+
+const Jeux = () => {
+    return (
+        <div>
+        <Header />
+        <h1>Jeux</h1>
+        <Game />
+        </div>
+    );
+    }
+
+export default Jeux;
