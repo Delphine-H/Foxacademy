76053816e58e61960f95480e6aa0bbcedfccd690
Delphine HANNON <hannon.delphine@gmail.com>
2024-10-31 11:23:41 +0100

    Parse score in integer.

--- frontend/src/utils/appUtils.js
+++ frontend/src/utils/appUtils.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 export const fetchUserScore = async () => {
   try {
-    const response = await axios.get(`http://localhost:5000/profile/score`, {
+    const response = await axios.get(`http://localhost:5000/game/score`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     });
-    return response.data.score;
+    const score = parseInt(response.data.score);
+    console.log('Score de l\'utilisateur:', score);
+    return score;
   } catch (error) {
     console.error('Erreur lors de la récupération du score de l\'utilisateur:', error);
   }
