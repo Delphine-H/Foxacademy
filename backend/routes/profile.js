840dacae26670de6a691310c02544a43ab425baa
Delphine HANNON <hannon.delphine@gmail.com>
2024-11-03 17:39:17 +0100

    Add profil features (picture caroussel, get datas and put datas)

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -7,7 +7,9 @@ const authenticateJWT = require('../middleware/authenticateJWT'); // JWT authent
 // GET: Retrieve user profile
 router.get('/', authenticateJWT, async (req, res) => {
   try {
-    const user = await User.findByPk(req.user.UserID);
+    const user = await User.findByPk(req.user.UserID, {
+      attributes: ['Firstname', 'Name', 'Email', 'Dob', 'Role', 'Level', 'Avatar']
+    });
     if (user) {
       res.json(user);
     } else {
@@ -43,6 +45,25 @@ router.put('/', authenticateJWT, async (req, res) => {
   } catch (err) {
     console.error('Error updating profile:', err);
     res.status(500).json({ error: 'Failed to update profile' });
+  }
+});
+
+// PUT: Change password
+router.put('/change-password', authenticateJWT, async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  try {
+    const user = await User.findByPk(req.user.UserID);
+    if (bcrypt.compareSync(oldPassword, user.Password)) {
+      const hashedPassword = bcrypt.hashSync(newPassword, 10);
+      await User.update({ Password: hashedPassword }, { where: { UserID: req.user.UserID } });
+      res.json({ message: 'Password updated successfully' });
+    } else {
+      res.status(400).json({ message: 'Invalid password' });
+    }
+  } catch (err) {
+    console.error('Error changing password:', err);
+    res.status(500).json({ error: 'Failed to change password' });
   }
 });
 
