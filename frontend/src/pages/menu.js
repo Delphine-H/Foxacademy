a32e98c9bd958cecb22e162d5c395809bcb6cb7b
Stefani-web <stephaniecvlho@gmail.com>
2024-11-03 01:00:07 +0100

    Fix content menu illustration and fix style

--- frontend/src/pages/menu.js
+++ frontend/src/pages/menu.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/menu.css';
 import logoFox from '../assets/Logo_Fox.png';
+import defaultProfile from '../assets/default-profil.png';
+import progressIcon from '../assets/progres.png';
+import quizIcon from '../assets/quiz.png';
+import gameIcon from '../assets/gaming.png';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import '../styles/footer.css';
@@ -40,19 +44,19 @@ const Menu = () => {
         <h1>Menu</h1>
         <div className="card-grid">
           <div className="card" onClick={handleProfilNavigation}>
-            <img src="fox-profil.png" alt="Profil" className="card-icon" />
+          <img src={defaultProfile} alt="Profil" className="card-icon" />
             <p>Profil</p>
           </div>
           <div className="card" onClick={handleProgressionNavigation}>
-            <img src="progress-icon.png" alt="Progression" className="card-icon" />
+          <img src={progressIcon} alt="Progression" className="card-icon" />
             <p>Progression</p>
           </div>
           <div className="card" onClick={handleQuizNavigation}>
-            <img src="quiz-icon.png" alt="Quizz" className="card-icon" />
+            <img src={quizIcon} alt="Quizz" className="card-icon" />
             <p>Quizz</p>
           </div>
           <div className="card" onClick={handleGameNavigation}>
-            <img src="game-icon.png" alt="Jeux" className="card-icon" />
+          <img src={gameIcon} alt="Jeux" className="card-icon" />
             <p>Jeux</p>
           </div>
         </div>
